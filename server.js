b52112485fb0a578975b652ad6ca050e48f202e7
Akhilesh <AkhileshM@outlook.in>
2019-08-20 15:09:49 +0530

    Updated call back

--- server.js
+++ server.js
@@ -2,4 +2,16 @@ const email = require('./_modules/email');
 
 const obj = { changePasswordLink: 'www.google.co.in' };
 
-email.sendEmail('akhilesh.g2t@gmail.com', 'Test HTML Template', './email/templates/forgotEmail.template.ejs', obj);
+email.sendEmail(
+  'akhilesh.g2t@gmail.com',
+  'Test HTML Template',
+  './email/templates/forgotEmail.template.ejs',
+  obj,
+  (err, info) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('Message sent: %s', info.messageId);
+    }
+  }
+);
