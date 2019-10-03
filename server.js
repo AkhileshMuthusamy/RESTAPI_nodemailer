cf9259d93faef681119280b152800ad9c08e2f38
Akhilesh <AkhileshM@outlook.in>
2019-10-03 20:09:02 +0530

    Added ability to receive multiple files

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var upload = multer({
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(upload.array());
+app.use(upload.array('allfile', 12));
 
 app.post('/api/forgotPassword', (req, res) => {
   const emailTemplateData = { changePasswordLink: 'www.google.co.in' };
@@ -36,10 +36,30 @@ app.post('/api/emailAttachment', (req, res) => {
   const toAddress = 'akhilesh.g2t@gmail.com';
   const subject = 'Test attachment';
 
-  console.log(req.body.pdfFile);
+  // console.log(req.body.pdfFile);
+  console.dir(req.files);
 
   mailer
     .sendEmailwithAttachment(toAddress, subject, 'Body of the message', req.body.pdfFile)
+    .then(info => {
+      console.log('Message sent: %s', info.messageId);
+      res.status(200).json({ result: 'Email sent successfully', message: info.messageId });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ Error: 'Internal error. Unable to proccess your request' });
+    });
+});
+
+app.post('/api/emailAttachments', (req, res) => {
+  const toAddress = 'akhilesh.g2t@gmail.com';
+  const subject = 'Test attachments';
+
+  // console.log(req.body.pdfFile);
+  console.dir(req.files);
+
+  mailer
+    .sendEmailwithAttachments(toAddress, subject, 'Body of the message', req.files)
     .then(info => {
       console.log('Message sent: %s', info.messageId);
       res.status(200).json({ result: 'Email sent successfully', message: info.messageId });
